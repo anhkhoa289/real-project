5367e1ca42b1207dce41e73d971cde77123d407b
gldev6713 <gldev6713@gmail.com>
2018-11-05 17:43:42 +0700

    Initial commit

---
+++ contracts/hello.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.18;
+// We have to specify what version of compiler this code will compile with
+contract Hello {
+  function hi() public pure returns (string) {
+    return ("Hi Khoa");
+  }
+}
